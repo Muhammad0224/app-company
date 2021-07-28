9a567bd35d088b688e7defe99e792551b7912266
muhammad0224 <murtazayevmuhammad@gmail.com>
2021-07-28 16:48:57 +0500

    first initial

---
+++ src/main/java/uz/pdp/appcompany/AppCompanyApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCompanyApplication.class, args);
+    }
+
+}
